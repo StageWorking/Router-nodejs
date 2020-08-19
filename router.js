cc729cd2e8758457ac413431bc5266b1bd64a162
h5mcbox <33593244+h5mcbox@users.noreply.github.com>
2020-08-19 13:06:25 +0800

    Update router.js

--- router.js
+++ router.js
@@ -14,13 +14,13 @@ const router=function(url,res, body = route["/404"], status = 200, headers = {})
         }
         else {
             res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end(body)
+            res.end(body);
         }
     }
     catch (e) {
         if (route["/500"]) {
-            var func = route["/500"](url, e.name, e.message);
-            res.writeHead.apply(this, func[1]);
+            var func = route["/500"](url, e);
+            res.writeHead(func[1]["status"],func[1]["headers"]);
             res.end(func[0]);
             return null;
         }
@@ -28,4 +28,4 @@ const router=function(url,res, body = route["/404"], status = 200, headers = {})
         res.end(`Web server gots some error.\n${e.name} ${e.message}`);
     }
 }
-module.exports=router
+module.exports=router;
